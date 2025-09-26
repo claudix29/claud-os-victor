ff9c3135309e75d261220842d90a8a2c3801a339
kercre123 <kerigancreighton@gmail.com>
2025-09-26 15:31:54 -0500

    Add message to .clangd telling you where the original file is, add unused-includes to suppression list

--- tools/build/build-scripts/gen-clangd.sh
+++ tools/build/build-scripts/gen-clangd.sh
@@ -6,5 +6,6 @@ export GIT_PROJ_ROOT="$(
 )"
 
 cd "${GIT_PROJ_ROOT}"
-cp tools/build/build-scripts/clang-template .clangd
+echo '#DO NOT MODIFY THIS FILE. IF YOU WANT DIFFERENT DIAGNOSTICS, MODIFY tools/build/build-tools/clangd-template, then rebuild' > .clangd
+cat tools/build/build-scripts/clangd-template >> .clangd
 sed -i "s|HOME_REPLACE|$HOME|g" .clangd
